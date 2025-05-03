4ce04f31e72e59abfc828ed49d9aa883e95228a9
risalstr <risa.lestari2002@gmail.com>
2025-05-03 11:15:12 +0700

    fix:fix error

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-    /* config options here */
     reactStrictMode: true,
     images: {
-        domains: ['res.cloudinary.com', 'pbs.twimg.com', '0205-202-158-77-58.ngrok-free.app', 'cdn.ngrok.com'],
+        domains: ['res.cloudinary.com', 'https://res.cloudinary.com'], // tanpa spasi atau karakter aneh
     },
 };
+
+module.exports = nextConfig;

e2b3b12ad3cfed6052215952a5b35ee933cceebc
risalstr <risa.lestari2002@gmail.com>
2025-05-03 08:04:38 +0700

    fix:fix error

---
+++ next.config.js
@@ -0,0 +1,7 @@
+const nextConfig = {
+    /* config options here */
+    reactStrictMode: true,
+    images: {
+        domains: ['res.cloudinary.com', 'pbs.twimg.com', '0205-202-158-77-58.ngrok-free.app', 'cdn.ngrok.com'],
+    },
+};
